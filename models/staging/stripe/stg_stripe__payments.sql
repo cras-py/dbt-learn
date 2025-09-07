89105cc6662cda8e63276512d20969316c327cd4
Corey Rastello <crastello.py@gmail.com>
2025-09-07 17:00:43 +0000

    refactored staging models to be built from code gen

--- models/staging/stripe/stg_stripe__payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount / 100 as amount,
-    created as created_at,
-    _batched_at as batched_at
-from raw.stripe.payment
